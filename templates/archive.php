a02ab61b58b4b677e5711c64f9129f97208bdcd4
ngoth <ngothuong33.hanu@gmail.com>
2016-08-11 18:33:37 +0700

    completed the archive page

--- templates/archive.php
+++ templates/archive.php
@@ -4,7 +4,6 @@ Template Name: Archive
 */
 get_header(); ?>
 <div class="container">
-
 	<div class="row">		
 		<div class="col-md-12">
 			<article class="archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -13,18 +12,36 @@ get_header(); ?>
 							<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 					<?php endwhile; // end of the loop. ?>
 				</header><!-- .entry-header -->
-				<div class="entry-content">
+				<div class="entry-content">	
 					<?php the_content(); ?>
-					
-					<div class="author">
-						<h1>Author</h1>
-						<?php wp_list_authors( 'exclude_admin=0' ); ?>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="left">
+								<div class="author">
+									<h1>Author</h1>
+									<?php wp_list_authors( 'exclude_admin=0' ); ?>
+								</div>
+								<div class="catgories">
+									<h1>Catgories</h1>
+									<?php wp_list_categories('title_li='); ?>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="right">
+								<div class="latest-post">
+									<h1>Latest Post</h1>
+									<?php wp_get_archives('type=postbypost&limit=5'); ?>
+								</div>
+								<div class="post-date">
+									<h1>Posts by month</h1>
+									<?php wp_get_archives(); ?>
+								</div>
+							</div>
+						</div>
 					</div>
-					
-
 				</div><!-- .entry-content -->
 			</article><!-- #post-## -->
-
 		</div>
 	</div>
 </div>
